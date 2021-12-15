7631b2ea0089e1f7c44d70e7a42627a5df299c18
Albert Pangestu <stamina.left@gmail.com>
2021-12-15 22:02:50 +0700

    Create a new test to check whether it receives notifications for two times in two seconds.

---
+++ TestingAsyncUsingExpectationsTests/TestingMultipleExpectationsTests.swift
@@ -0,0 +1,25 @@
+//
+//  TestingMultipleExpectationsTests.swift
+//  TestingAsyncUsingExpectationsTests
+//
+//  Created by Albert Pangestu on 15/12/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import XCTest
+
+class TestingMultipleExpectationsTests: XCTestCase {
+
+    func test_MultipleNotification() {
+        
+        let alertManager = AlertManager()
+        let exp = expectation(forNotification: Notification.Name.alertNotification, object: Notification, handler: nil)
+        
+        exp.expectedFulfillmentCount = 2
+        
+        alertManager.postAlert()
+        
+        wait(for: [exp], timeout: 2.0)
+    }
+
+}
