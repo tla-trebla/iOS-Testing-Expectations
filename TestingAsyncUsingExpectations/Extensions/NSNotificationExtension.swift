9ce477bbb8b767fb99acea53b0cea45b0b5a499b
Albert Pangestu <stamina.left@gmail.com>
2021-12-15 22:25:09 +0700

    Created 'alertNotification' from extension of Notification.Name.

---
+++ TestingAsyncUsingExpectations/Extensions/NSNotificationExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NSNotificationExtension.swift
+//  TestingAsyncUsingExpectations
+//
+//  Created by Albert Pangestu on 15/12/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let alertNotification = NSNotification.Name.init(rawValue: "alertNotification")
+}
